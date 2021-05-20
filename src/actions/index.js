fc668694b35c721e2f65124ea983551e1726e658
saj <aejeong-context@kakao.com>
2021-05-20 09:54:29 +0900

    [add] store 생성
    주로 미들웨어를 설정하는 일을 한다.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
-import {SET_COLOR} from '/ActionType';
+import * as types from './ActionType';
 
 // 액션 생성자 - 함수, 리턴 - 객체
 
 
 export function setColor(color){
-    return(
-        type: SET_COLOR,
+    return{
+        type: types.SET_COLOR,
         color
-    );
+    };
 
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { createStore } from 'redux';
+// reducers를 합쳐놓은 index 가지고옴
+import reducers from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+
+// 스토어 생성 -> 애플리케이션 오직 1개
+// dispatch(action) -> action을 reducers에 전달
+// getState() -> 현재 상태 반환
+// subscribe -> 상태가 바뀔 때마다 실행할 함수를 등록
+const store = createStore(reducers);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
